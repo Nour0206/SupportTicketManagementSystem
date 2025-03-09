42ee3a56dd0d32d8190709361d0d97475fb2c847
afwakh <afwakh78@gmail.com>
2025-03-09 01:23:12 +0100

    feat: add filtering by createdBy and assignedTo in TicketController and TicketRoute, fix related bugs

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -6,7 +6,10 @@ const {
     getTicketByName,
     updateTicketStatus,
     updateTicket,
-    deleteTicket
+    deleteTicket,
+    filterByStatus,
+    getTicketsByAgentId,
+    getTicketsByUserId
 } = require('../controllers/ticketController');
 
 const router = express.Router();
@@ -14,10 +17,14 @@ const router = express.Router();
 // Define routes
 router.post('/addTicket', createTicket);
 router.get('/all', getAllTickets);
+router.get('/status/:status', filterByStatus);
 router.get('/:id', getTicketById);
 router.get('/name/:name', getTicketByName);
 router.put('/:id', updateTicket);
 router.patch('/:id/status', updateTicketStatus);
 router.delete('/:id', deleteTicket);
+router.get('/agent/:userId', getTicketsByAgentId);
+router.get('/user/:userId', getTicketsByUserId);
+
 
 module.exports = router;
